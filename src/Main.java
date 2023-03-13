df180b8479104544005d60e34c45679a7a8387ab
13tom13 <13tom13@mail.ru>
2023-03-13 11:31:47 +0200

    max task

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,64 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
         long startTs = System.currentTimeMillis(); // start time
-        List<Thread> threads = new ArrayList<>();
+        List<Future<Integer>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(texts.length);
+        int max = 0;
+
         for (int i = 0; i < texts.length; i++) {
             int b = i;
-//            threads.add(new Thread(() -> Main.sreach(texts[b])));
-            threads.add(new Thread(() -> sreach(texts[b])));
-        }
-            for (Thread t : threads) {
-                t.start();
-                t.join();
-            }
-            long endTs = System.currentTimeMillis(); // end time
-            System.out.println("Time: " + (endTs - startTs) + "mds");
+            Future<Integer> future = executor.submit( () -> sreach(texts[b]));
+            futures.add(future);
         }
 
-        public static String generateText (String letters,int length){
-            Random random = new Random();
-            StringBuilder text = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                text.append(letters.charAt(random.nextInt(letters.length())));
-            }
-            return text.toString();
+        for (Future<Integer> future : futures) {
+           max = Math.max(max, future.get());
         }
 
-        public static void sreach (String text){
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
+        long endTs = System.currentTimeMillis(); // end time
+        
+        System.out.println();
+        System.out.println("Max: " + max);
+        System.out.println("Time: " + (endTs - startTs) + "mds");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    public static Integer sreach(String text) {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
                     }
                 }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
             }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
         }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        return maxSize;
     }
+}
